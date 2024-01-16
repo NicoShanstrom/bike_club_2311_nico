b99d774e50cc8bf61bf3c76f0d92fe60a5bec146
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-01-16 12:00:39 -0500

    Make test for #log_ride(ride, time)

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -21,5 +21,26 @@ RSpec.describe Biker do
             expect(biker.acceptable_terrain).to eq([:gravel, :hills])
         end
     end
+
+    describe 'log_ride(ride, time)' do
+        it 'can keep track of all of its previous rides and times for those rides' do
+            biker = Biker.new("Kenny", 30)
+            biker.learn_terrain!(:gravel)
+            biker.learn_terrain!(:hills)
+            ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, 
+                            loop: false, terrain: :hills})
+            ride2 = Ride.new({name: "Town Lake", distance: 14.9, 
+                            loop: true, terrain: :gravel})
+            biker.log_ride(ride1, 92.5)
+            biker.log_ride(ride1, 91.1)
+            biker.log_ride(ride2, 60.9)
+            biker.log_ride(ride2, 61.6)
+
+            expect(biker.rides).to eq({ ride1 => [92.5, 91.1], ride2 => [60.9, 61.6]})
+
+
+
+        end
+    end
     
 end
