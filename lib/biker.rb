b8a6ecbc67066698e6343410b11c40760eb34869
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-01-16 13:34:55 -0500

    Make tests pass with methods, modify methods

--- lib/biker.rb
+++ lib/biker.rb
@@ -16,13 +16,16 @@ class Biker
     end
 
     def log_ride(ride, time)
-        if @acceptable_terrain.include?(ride.terrain) && ride.distance <= @max_distance 
-            if @rides.key?(ride)  
-                @rides[ride] << time 
-            else
-                @rides[ride] = [time]
-            end
+        if acceptable_terrain.include?(ride.terrain) && ride.total_distance <= max_distance
+            rides[ride] = [] if rides[ride].nil?
+            rides[ride] << time
+        else
+            puts "Cannot log ride due to terrain & max distance"
         end
+    end
+
+    def personal_record(ride)
+        rides[ride]&.min || false
     end
     # require 'pry'; binding.pry
 end
