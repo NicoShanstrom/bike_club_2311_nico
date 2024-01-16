14c32b777b4d520dbd039b6c1a9dcdeaca7d6fc1
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-01-16 11:28:30 -0500

    Create initialize and loop? methods

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,16 @@
 class Ride
+attr_reader :name, :distance, :terrain,
+            :loop, :total_distance
+    def initialize(info)
+    @name = info[:name]
+    @distance = info[:distance]
+    @terrain = info[:terrain]
+    @loop = info[:loop]
+    @total_distance = info[:total_distance]
 
+    end
+
+    def loop?
+        @loop
+    end
 end
